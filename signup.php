b43a2f19449a87d7afe308f50c494bf5ccc100a4
bpeightal <bpeightal@unomaha.edu>
2023-04-23 08:44:40 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,12 +7,14 @@ $email = $_POST["email"];
 $teacher = $_POST["teacher"];
 $notes = $_POST["notes"];
   
-$host = "";
+$host = "meetinginfo.cgj7os1xcd9b.us-east-1.rds.amazonaws.com";
 $dbname = "bpeightal1";
 $username = "bpeightal";
-$password = "";
+$password = "unoproject";
 
 mysqli_connect($host, $username, $password, $dbname);
 
 $sql = "INSERT INTO signup (name, date, time, email, teacher, notes)
         VALUES ('$name', $date, '$time', '$email', '$teacher', '$notes')";
+
+?>
