d24f0d0253d282bd60adabaa74802abdc0f4ba18
bpeightal <bpeightal@unomaha.edu>
2023-04-24 15:57:16 -0500

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,18 +1,18 @@
 <?php
 
-$name = $_POST["name"];
-$date = $_POST["date"];
-$time = $_POST["time"];
-$email = $_POST["email"];
-$teacher = $_POST["teacher"];
-$notes = $_POST["notes"];
+$name = $_POST['name'];
+$date = $_POST['date'];
+$time = $_POST['time'];
+$email = $_POST['email'];
+$teacher = $_POST['teacher'];
+$notes = $_POST['notes'];
   
-$host = "meetinginfo.cgj7os1xcd9b.us-east-1.rds.amazonaws.com";
-$dbname = "bpeightal1";
-$username = "bpeightal";
-$password = "unoproject";
+$con=mysqli_connect('meetinginfo.cgj7os1xcd9b.us-east-1.rds.amazonaws.com', 'bpeightal', 'unoproject', 'bpeightal1');
 
-mysqli_connect($host, $username, $password, $dbname);
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
 
 $sql = "INSERT INTO signup (name, date, time, email, teacher, notes)
         VALUES ('$name', $date, '$time', '$email', '$teacher', '$notes')";
